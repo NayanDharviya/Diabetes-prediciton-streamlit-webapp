9cafb7c79c78d4e289da80a4fd9ce77c4c2a57e5
nayandharviya <nd8137@gmail.com>
2021-05-29 12:31:58 +0530

    First pushing

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"n.itvedant@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
